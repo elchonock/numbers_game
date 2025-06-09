8c599a0546baa40d3b01a66d49223d6051609a42
elchonock <elya3029@gmail.com>
2025-06-09 18:35:21 +0200

    fix: update screens

--- screens/game-over/screen.tsx
+++ screens/game-over/screen.tsx
@@ -1,3 +1,11 @@
-const GameOverScreen = () => {};
+import MyText from '@/ui/text';
+
+const GameOverScreen = () => {
+  return (
+    <MyText color="white" size="2xl" weight="600">
+      Game Over
+    </MyText>
+  );
+};
 
 export default GameOverScreen;
--- screens/game/screen.tsx
+++ screens/game/screen.tsx
@@ -1,3 +1,11 @@
-const GameScreen = () => {};
+import MyText from '@/ui/text';
+
+const GameScreen = () => {
+  return (
+    <MyText color="white" size="2xl" weight="600">
+      Game
+    </MyText>
+  );
+};
 
 export default GameScreen;
