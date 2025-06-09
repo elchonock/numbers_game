da84600fc6d517f3f20b5dbd16bdeaa4e6f55f24
elchonock <elya3029@gmail.com>
2025-06-09 18:35:58 +0200

    add: numeric input component

---
+++ ui/input/index.tsx
@@ -0,0 +1,45 @@
+import { FONT_FAMILY } from '@/ui/text';
+import { THEME_COLORS } from '@/ui/theme';
+import { StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+
+export interface MyNumericInputProps extends TextInputProps {
+  color?: string;
+  size?: string;
+}
+
+function NumericInput({ style = {}, ...props }: MyNumericInputProps) {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={[styles.input, style]}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCorrect={false}
+        cursorColor={THEME_COLORS.ALL.purple}
+        {...props}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+  },
+  input: {
+    textAlign: 'center',
+    color: THEME_COLORS.ALL.orange,
+    fontFamily: FONT_FAMILY[600],
+    fontSize: 32,
+    fontWeight: 500,
+    borderWidth: 1,
+    borderBottomWidth: 4,
+    borderColor: THEME_COLORS.ALL.orange,
+    borderRadius: 999,
+    padding: 12,
+    width: '100%',
+    backgroundColor: THEME_COLORS.ALL.white,
+  },
+});
+
+export default NumericInput;
