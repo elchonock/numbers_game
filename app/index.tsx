dedc687cf2b65ffa2488a2c3f5135372b1c0df46
elchonock <elya3029@gmail.com>
2025-09-14 22:29:36 +0200

    upd: logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { GAME_SETTINGS } from '@/data/constants';
 import GameOverScreen from '@/screens/game-over/screen';
 import GameScreen from '@/screens/game/screen';
 import StartScreen from '@/screens/start/screen';
@@ -19,6 +20,8 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
+const getRandomNumber = (from: number, to: number) => Math.floor(Math.random() * (to - from + 1)) + from;
+
 SplashScreen.preventAutoHideAsync();
 
 const OXANIUM = {
@@ -33,17 +36,24 @@ const OXANIUM = {
 
 export type OxaniumFontVariants = keyof typeof OXANIUM;
 
-type GameProgress = 'notStarted' | 'inProgress' | 'finished';
 type Screens = 'start' | 'game' | 'gameOver';
+export type GuessType = 'lower' | 'greater';
 
-type GameState = {
-  progress: GameProgress;
-  screen: Screens;
+export type GameState = {
   attempts: number;
-  guesses: number[];
+  previousGuesses: number[];
+  currentGuess: number;
+  min: number;
+  max: number;
 };
 
-const initialGameState: GameState = { progress: 'notStarted', screen: 'start', attempts: 0, guesses: [] };
+const initialGameState: GameState = {
+  currentGuess: 12,
+  attempts: 0,
+  previousGuesses: [],
+  min: GAME_SETTINGS.MIN,
+  max: GAME_SETTINGS.MAX,
+};
 
 export default function Index() {
   const [fontsLoaded, fontsError] = useFonts({
@@ -53,16 +63,124 @@ export default function Index() {
 
   const [gameState, setGameState] = useState<GameState>(initialGameState);
   const [theNumber, setTheNumber] = useState<number | null>(null);
+  const [currentScreen, setCurrentScreen] = useState<Screens>('start');
 
-  const isNumberValid = (number: number | string | null) => {
-    return !!(typeof number === 'number' && !isNaN(+number) && +number > 0 && +number < 100);
+  const makeNewGuess = (min: number, max: number) => {
+    if (min >= max) return min;
+
+    let newGuess = getRandomNumber(min, max);
+
+    if (newGuess === theNumber) {
+      return newGuess;
+    }
+
+    if (gameState.previousGuesses.includes(newGuess)) {
+      newGuess = makeNewGuess(min, max);
+    }
+
+    return newGuess;
   };
 
-  useEffect(() => {
-    if (isNumberValid(theNumber)) {
-      setGameState((prev) => ({ ...prev, progress: 'inProgress', screen: 'game' }));
+  const guessLower = () => {
+    if (theNumber && gameState.currentGuess < theNumber) {
+      console.log('Liar, liar, pants on fire!'); // to do
+      return;
     }
-  }, [theNumber]);
+
+    const newMin = gameState.min;
+    const newMax = gameState.currentGuess;
+
+    const newCurrentGuess = makeNewGuess(newMin, newMax);
+
+    if (newCurrentGuess === theNumber) {
+      setGameState((prev) => ({ ...prev, attempts: prev.attempts + 1, currentGuess: newCurrentGuess }));
+      setCurrentScreen('gameOver');
+    }
+
+    setGameState((prev) => ({
+      ...prev,
+      currentGuess: newCurrentGuess,
+      attempts: prev.attempts + 1,
+      previousGuesses: [newCurrentGuess, ...prev.previousGuesses],
+      min: newMin,
+      max: newMax,
+    }));
+  };
+
+  const guessHigher = () => {
+    if (theNumber && gameState.currentGuess > theNumber) {
+      console.log('Liar, liar, pants on fire!'); // to do
+      return;
+    }
+
+    const newMin = gameState.currentGuess;
+    const newMax = gameState.max;
+
+    const newCurrentGuess = makeNewGuess(newMin, newMax);
+
+    if (newCurrentGuess === theNumber) {
+      setGameState((prev) => ({ ...prev, attempts: prev.attempts + 1, currentGuess: newCurrentGuess }));
+      setCurrentScreen('gameOver');
+    }
+
+    setGameState((prev) => ({
+      ...prev,
+      currentGuess: newCurrentGuess,
+      attempts: prev.attempts + 1,
+      previousGuesses: [newCurrentGuess, ...prev.previousGuesses],
+      min: newMin,
+      max: newMax,
+    }));
+  };
+
+  const makeInitialGuess = () => {
+    const getFirstGuess = (min: number, max: number) => {
+      if (min === max) return min;
+
+      let firstGuess = getRandomNumber(min, max);
+
+      if (firstGuess === theNumber) {
+        firstGuess = getFirstGuess(min, max);
+      }
+      return firstGuess;
+    };
+
+    setGameState((prev) => {
+      const newCurrentGuess = getFirstGuess(prev.min, prev.max);
+
+      return {
+        ...prev,
+        currentGuess: newCurrentGuess,
+        attempts: prev.attempts + 1,
+        previousGuesses: [newCurrentGuess, ...prev.previousGuesses],
+      };
+    });
+
+    return;
+  };
+
+  const isNumberValid = (number: number | null) => {
+    return !!(
+      typeof number === 'number' &&
+      !isNaN(+number) &&
+      number >= GAME_SETTINGS.MIN &&
+      number <= GAME_SETTINGS.MAX
+    );
+  };
+
+  const submitNumber = (number: typeof theNumber) => {
+    if (isNumberValid(number)) {
+      setTheNumber(number);
+      makeInitialGuess();
+      setCurrentScreen('game');
+    }
+  };
+
+  const startOver = () => {
+    setGameState(initialGameState);
+    setTheNumber(null);
+    setCurrentScreen('start');
+  };
 
   useEffect(() => {
     if (fontsLoaded || fontsError) {
@@ -73,10 +191,13 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      {currentScreen === 'start' && <StartScreen setNumber={submitNumber} />}
 
-      {gameState.screen === 'start' && <StartScreen setNumber={setTheNumber} />}
-      {gameState.screen === 'game' && <GameScreen />}
-      {gameState.screen === 'gameOver' && <GameOverScreen />}
+      {currentScreen === 'game' && (
+        <GameScreen gameState={gameState} guessLower={guessLower} guessHigher={guessHigher} />
+      )}
+
+      {currentScreen === 'gameOver' && <GameOverScreen gameState={gameState} startOver={startOver} />}
     </View>
   );
 }
--- screens/game/screen.tsx
+++ screens/game/screen.tsx
@@ -1,11 +1,100 @@
+import { GameState } from '@/app';
+import Guess from '@/components/guess';
+import SubTitle from '@/components/subtitle';
+import Title from '@/components/title';
+import MyButton from '@/ui/button';
+import List from '@/ui/list';
 import MyText from '@/ui/text';
+import { THEME_COLORS } from '@/ui/theme';
+import { MaterialIcons } from '@expo/vector-icons';
+import { nanoid } from 'nanoid';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import 'react-native-get-random-values';
 
-const GameScreen = () => {
+interface GameScreenProps {
+  gameState: GameState;
+  guessLower: () => void;
+  guessHigher: () => void;
+}
+
+const GameScreen = ({ gameState, guessLower, guessHigher }: GameScreenProps) => {
+  const prevAttemptsList = gameState.previousGuesses.map((g, i) => ({
+    id: nanoid(),
+    title: g,
+    number: gameState.previousGuesses.length - i,
+  }));
+
   return (
-    <MyText color="white" size="2xl" weight="600">
-      Game
-    </MyText>
+    <View style={styles.container}>
+      <Title color="red">PLAY</Title>
+      <SubTitle style={{ width: '100%' }}>I will guess your number!</SubTitle>
+
+      <View style={styles.guessContainer}>
+        <SubTitle style={{ width: '100%' }}>Is your number lower or greater than?</SubTitle>
+        <View style={styles.buttonsContainer}>
+          <MyButton onPress={guessLower} iconName="squared-minus" color="white" style={styles.button} />
+
+          <Guess guess={gameState.currentGuess} />
+
+          <MyButton onPress={guessHigher} iconName="squared-plus" color="white" style={styles.button} />
+        </View>
+      </View>
+
+      <View style={styles.attemptsContainer}>
+        <View style={styles.attempts}>
+          <MaterialIcons name="history" size={24} color="white" />
+          <MyText color="white" size="xl" weight="500">
+            Previous attempts:
+          </MyText>
+        </View>
+
+        <List items={prevAttemptsList.slice(1)} />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+    gap: 24,
+    alignItems: 'center',
+    padding: 16,
+  },
+  guessContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 24,
+    backgroundColor: THEME_COLORS.ALL.blue,
+    borderRadius: 12,
+    padding: 20,
+    paddingVertical: 24,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 12,
+  },
+  attemptsContainer: {
+    gap: 16,
+    paddingTop: 20,
+    borderTopWidth: 2,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: THEME_COLORS.ALL.blue,
+    flex: 1,
+  },
+  attempts: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+  button: {},
+});
 
 export default GameScreen;
